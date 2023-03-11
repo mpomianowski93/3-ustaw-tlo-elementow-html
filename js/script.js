b1f9c477ddc0ceda2b408e84ecc1de8728534489
mpomianowski93 <124494099+mpomianowski93@users.noreply.github.com>
2023-03-11 10:00:35 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+const setBackground = () => {
+  let p1 = document.querySelector('.first');
+  let p2 = document.querySelector('.last');
+
+p1.classList.add('bg-red');
+p2.classList.add('bg-yellow');
+
+}
+
+let btnSetBackground = document.getElementById('set-background');
+
+btnSetBackground.addEventListener('click', setBackground);
